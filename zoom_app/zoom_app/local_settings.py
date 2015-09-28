ae1d94d630ed4027a69e229553e2a7af8bfd30b4
SummerLyn <s.lynbryant@gmail.com>
2015-09-28 14:46:22 -0700

    Added local_settings file

---
+++ zoom_app/zoom_app/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zoomapp_db',
+        'USER': 'summerlyn',
+        'PASSWORD': 'Bigredpsql9!',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
