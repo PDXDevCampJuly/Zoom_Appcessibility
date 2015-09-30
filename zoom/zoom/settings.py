795b4993e6be02ae8e32078af260099c352d4047
SummerLyn <s.lynbryant@gmail.com>
2015-09-29 17:20:11 -0700

    Added apps to settings

--- zoom/zoom/settings.py
+++ zoom/zoom/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'zoom',
+    'accessibilityneed',
+    'property',
+    'user',
 )
 
 MIDDLEWARE_CLASSES = (
